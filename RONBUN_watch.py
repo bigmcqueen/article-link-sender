d0ed2d1951ae0b9b98133502edaac8a886ab14ac
BigMcQueen <mmmrym10@gmail.com>
2022-10-03 18:37:28 +0900

    update RONBUN_watch.py

--- RONBUN_watch.py
+++ RONBUN_watch.py
@@ -9,13 +9,13 @@ target_url = f'https://aasj.jp/date/{date_today.year}/{date_today.month:02}/{dat
 
 r = requests.get(target_url)
 soup = BeautifulSoup(r.content, 'html.parser')
-title = soup.find('h2').get_text()
+target_title = soup.find('h2').get_text()
 
 # The token is stored in 'token' in line_token.py
 TOKEN = line_token.token
 
 api_url = 'https://notify-api.line.me/api/notify'
-send_contents = [title, target_url]
+send_contents = [target_title, target_url]
 
 TOKEN_dic = {'Authorization': 'Bearer' + ' ' + TOKEN}
 send_dic = {'message': send_contents}
